6ddca346b20cce04e25757b7b203b93fda2e9193
Michael Lee <mzlee@cs.utexas.edu>
2011-09-01 14:03:07 -0500

    Small bug in bash_aliases Changed install to properly handle directories

--- install.sh
+++ install.sh
@@ -6,15 +6,22 @@ FILES=${@:-${DEFAULT_FILES}}
 
 for FILE in ${FILES}
 do
+    need_link=1
     if [ -f ${HOME}/.${FILE} ]
     then
 	`diff -w -q ${HOME_ENV}/${FILE} ${HOME}/.${FILE} > /dev/null`
+	need_link=${?}
 	if [ ${?} -eq 1 ]
 	then
 	    mv ${HOME}/.${FILE} ${HOME}/.${FILE}-
-	    ln -s ${HOME_ENV}/${FILE} ${HOME}/.${FILE}
 	fi
-    else
+    elif [ -d ${HOME}/.${FILE} ]
+    then
+	rm ${HOME}/.${FILE}
+    fi
+
+    if [ ${need_link} -eq 1 ]
+    then
 	ln -s ${HOME_ENV}/${FILE} ${HOME}/.${FILE}
     fi
 done
