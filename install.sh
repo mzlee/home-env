f83539b3c14834f14c7b2741a6bf05c63e4d8794
Michael Lee <mzlee@cs.utexas.edu>
2015-02-03 15:42:33 -0800

    [install] Fix directories.

--- install.sh
+++ install.sh
@@ -7,21 +7,17 @@ FILES=${@:-${DEFAULT_FILES}}
 for FILE in ${FILES}
 do
     need_link=1
-    if [ -f ${HOME}/.${FILE} ]
-    then
+    if [ -f ${HOME}/.${FILE} ]; then
 	`diff -w -q ${HOME_ENV}/${FILE} ${HOME}/.${FILE} > /dev/null`
 	need_link=${?}
-	if [ ${need_link} -eq 1 ]
-	then
+	if [ ${need_link} -eq 1 ]; then
 	    mv ${HOME}/.${FILE} ${HOME}/.${FILE}-
 	fi
-    elif [ -d ${HOME}/.${FILE} ]
-    then
-	rm ${HOME}/.${FILE}
+    elif [ -d ${HOME}/.${FILE} ]; then
+	mv ${HOME}/.${FILE} ${HOME}/.${FILE}-
     fi
 
-    if [ ${need_link} -eq 1 ]
-    then
+    if [ ${need_link} -eq 1 ]; then
 	ln -s ${HOME_ENV}/${FILE} ${HOME}/.${FILE}
     fi
 done
