731e1e8e6af5ea478236d9c4e2bb72585b3bf879
Michael Lee <mzlee@cs.utexas.edu>
2015-02-18 10:47:33 -0500

    [python] Update python tab complete to work.
    Silly OSX....

--- pythonrc.py
+++ pythonrc.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
-import rlcompleter
 import readline
-readline.parse_and_bind("tab: complete")
+import rlcompleter
+
+if 'libedit' in readline.__doc__:
+    readline.parse_and_bind("bind ^I rl_complete")
+else:
+    readline.parse_and_bind("tab: complete")
