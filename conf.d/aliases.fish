890b2f4ee693bc71e78c0f2678c7ac4e83edcb5c
Michael Lee <mzlee@umich.edu>
2022-12-21 23:26:52 -0500

    [fish/conf.d] Add aliases.fish

---
+++ conf.d/aliases.fish
@@ -0,0 +1,13 @@
+# Alias-like definitions
+
+function l --description 'Alias for ls -AlF'
+  ls -AlF
+end
+
+function la --description 'Alias for ls -AF'
+  ls -AF
+end
+
+function ll --description 'Alias for ls -l'
+  ls -l
+end
